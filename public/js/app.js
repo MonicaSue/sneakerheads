f8b1dcba0171512e6dc4ff01d433da600e71156d
Monica Sue <monicasue567@gmail.com>
2023-04-25 14:53:02 -0400

    update show css

--- public/js/app.js
+++ public/js/app.js
@@ -4,3 +4,8 @@ function menuOnClick() {
   document.getElementById("menu-bg").classList.toggle("change-bg");
 }
 
+function randomRoom() {
+  return Math.floor(Math.random() * (5-1 + 1) + 1)
+}
+
+console.log(randomRoom())
